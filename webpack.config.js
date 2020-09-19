b555e95f0bd7d0467abf72e1be3a3f7bddf3ea8a
Emi Martin <emimartin26@hotmail.com>
2020-09-19 15:44:39 -0300

    initial commit, configure webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve("src/client/index.js"),
+  output: {
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    open: true,
+    port: 9000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      filename: "./index.html"
+    }),
+  ]
+};
