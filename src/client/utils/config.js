6959552a1fa84de7b643cd50f4906106df1e332d
Emi Martin <emimartin26@hotmail.com>
2020-09-24 17:15:00 -0300

    configure redirect uri

--- src/client/utils/config.js
+++ src/client/utils/config.js
@@ -2,9 +2,14 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = process.env.CLIENT_ID;
-export const redirectUri = process.env.REDIRECT_URI;
+export const redirectUri =
+  process.env.NODE_ENV === "production"
+    ? location.origin
+    : "http://localhost:9000";
 export const scopes = [
   "user-top-read",
   "user-read-currently-playing",
   "user-read-playback-state",
 ];
+
+console.log(redirectUri)
